cadaa3c3c65866e1b0220a7efeb481511741c2d6
Dobos Zoltán <113709336+Pedro66Z@users.noreply.github.com>
2022-12-05 13:16:27 +0100

    Add files via upload

---
+++ 2.2.py
@@ -0,0 +1,11 @@
+
+lista = []
+c = None
+while c != "":
+    c = input("Adj meg kis a betűvel kezdődő szavakat\t")
+    if c != "":
+         print("")
+    for x in c:
+        if "a" in x  or "A" in x:
+            lista.append(c)
+print(lista)
